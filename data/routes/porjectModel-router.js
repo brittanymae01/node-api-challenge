46eb069574feec2702d75c0001bc74ba10940e39
brittanymae01 <brittanymae01>
2020-01-17 15:33:18 -0800

    cleaned up code

--- data/routes/porjectModel-router.js
+++ data/routes/porjectModel-router.js
@@ -78,37 +78,6 @@ router.post("/", (req, res) => {
     });
 });
 
-// for some reason my validation was not working ??? I get a weird error saying NOT NULL
-
-// router.post("/", (req, res) => {
-//   Projects.insert(req.body)
-//     .then(project => {
-//       if (!req.body.name || !req.body.description) {
-//         return res.status(400).json({
-//           errorMessage: "Please provide name and description for the user"
-//         });
-//       } else {
-//         //   if (!name) {
-//         //     return res.status(400).json({
-//         //       errorMessage: "please provide a name for the project"
-//         //     });
-//         //   }
-//         //   if (!description) {
-//         //     return res.status(400).json({
-//         //       errorMessage: "please provide a description for the project"
-//         //     });
-//         //   } else {
-//         return res.status(201).json(project);
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       return res.status(500).json({
-//         errorMessage: "problem adding project"
-//       });
-//     });
-// });
-
 // working!
 router.post("/:id/actions", (req, res) => {
   const projectInfo = { ...req.body, project_id: req.params.id };
